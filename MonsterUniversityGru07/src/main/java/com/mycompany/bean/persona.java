1541f7b15dc5caa33cc3d82a9b5342c70aeb4e30
Enrique Jimbo <enriok@hotmail.com>
2018-09-18 00:33:08 -0500

    Estructura Proyecto creado beans, servlets y clase persona

---
+++ MonsterUniversityGru07/src/main/java/com/mycompany/bean/persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bean;
+
+/**
+ *
+ * @author enrique
+ */
+import java.sql.Date;
+
+public class persona {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String documentoIdentidad;
+    private String correoElectronico;
+    private Date fechaNacimiento;
+    private Date fechaRegistro;
+    private Date fechaActualizacion;
+    
+}
