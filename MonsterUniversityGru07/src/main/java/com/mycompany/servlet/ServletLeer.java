c21d887a6c2c97426199da49db4f3050cfb4c6ef
Ximecis <ximeqr3b@gmail.com>
2018-09-18 13:46:59 -0500

    cambios en la estructura servletLeer.class

--- MonsterUniversityGru07/src/main/java/com/mycompany/servlet/ServletLeer.java
+++ MonsterUniversityGru07/src/main/java/com/mycompany/servlet/ServletLeer.java
@@ -5,8 +5,14 @@
  */
 package com.mycompany.servlet;
 
+import com.mycompany.bean.Persona;
+import com.mycompany.monsteruniversitygru07.Conexion;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,74 +21,74 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author enrique
+ * @authores Enrique,Ximena,Patricia,Elizabeth
  */
 @WebServlet(name = "ServletLeer", urlPatterns = {"/ServletLeer"})
 public class ServletLeer extends HttpServlet {
-
+	private static final long serialVersionUID = 1L;
+       
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @see HttpServlet#HttpServlet()
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletLeer</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletLeer at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public ServletLeer() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Conexion conexion=new Conexion();
+		
+		try
+		{
+			String sql="select * from tpersona";
+			
+			PreparedStatement prepareStatemente=(PreparedStatement)conexion.getConexion().prepareStatement(sql);
+			ResultSet resultSet=prepareStatemente.executeQuery();
+			
+			List<Persona> listaPersona=new ArrayList<Persona>();
+			
+			while(resultSet.next())
+			{
+				Persona persona=new Persona();
+				
+				persona.setId(resultSet.getInt("id"));
+				persona.setNombre(resultSet.getString("nombre"));
+				persona.setApellido(resultSet.getString("apellido"));
+				persona.setDocumentoIdentidad(resultSet.getString("documentoIdentidad"));
+				persona.setCorreoElectronico(resultSet.getString("correoElectronico"));
+				persona.setFechaNacimiento(resultSet.getDate("fechaNacimiento"));
+				persona.setFechaRegistro(resultSet.getDate("fechaRegistro"));
+				persona.setFechaActualizacion(resultSet.getDate("fechaActualizacion"));
+				
+				listaPersona.add(persona);
+			}
+			
+			prepareStatemente.close();
+			resultSet.close();
+			
+			request.setAttribute("listaPersona", listaPersona);
+			
+			request.getRequestDispatcher("leer.jsp").forward(request, response);
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		finally
+		{
+			conexion.cerrarConexion();
+		}
+	}
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
 
 }
